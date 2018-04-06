ddd70749e2211a25d28a2e8a1c7e73572c2e6def
wingeddreamer <wingedreamer@gmail.com>
2018-04-06 21:22:04 +0800

    加入关卡开头文字，激活“Level2_Begin_Text”或“Level3_Begin_Text”这两个GameObject即可自动播放。

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public Transform CameraRotator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnEnable()
+    {
+        transform.rotation = CameraRotator.rotation;
+    }
+}
