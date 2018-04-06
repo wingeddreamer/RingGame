89cf691c45eb1473f7e4120dc6f43ac879cf2de3
wingeddreamer <wingedreamer@gmail.com>
2018-04-06 12:15:18 +0800

    进入退出游戏模式的灯光控制 脚本在EnterExitPlayMode中，测试按键为A、 B

---
+++ Assets/EnterExitPlayMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterExitPlayMode : MonoBehaviour {
+
+    public Animator[] parts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+            foreach (var part in parts) if (part!=null) part.SetBool("Playmode", true);
+        if(Input.GetKeyDown(KeyCode.B))
+            foreach (var part in parts) if (part != null) part.SetBool("Playmode", false);
+    }
+}
