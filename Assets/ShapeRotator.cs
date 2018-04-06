0d07dbd0af975dfbc553481a0f848232fe0936c3
wingeddreamer <wingedreamer@gmail.com>
2018-04-06 20:00:13 +0800

    第一关的起始文字加入，按键开始游戏后需将底下“请按任意键开始挑战”字隐藏。

---
+++ Assets/ShapeRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeRotator : MonoBehaviour {
+
+    public bool CW;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, (CW?10:-10)*Time.deltaTime, 0));
+	}
+}
---
+++ Assets/StrobeLight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrobeLight : MonoBehaviour {
+
+    int timeInFrame = 0, interval = 2;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeInFrame>interval)
+        {
+            GetComponent<Light>().intensity = Random.Range(0, 5f);
+            interval = Random.Range(1, 8);
+            timeInFrame = 0;
+        }
+        timeInFrame++;
+    }
+}
