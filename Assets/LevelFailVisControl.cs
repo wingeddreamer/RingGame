dc4895990a89dd995306d5fd5d14912f8e50fa48
wingeddreamer <wingedreamer@gmail.com>
2018-04-08 22:41:32 +0800

    加入对于三个关卡所有物体的隐藏动画控制，在Scripts物体上，叫做LevelFailVisControl的脚本，下面有三个布尔量，当设为“假”时分别隐藏三关的对象，当设为“真”时，将对象显示出来。

---
+++ Assets/LevelFailVisControl.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelFailVisControl : MonoBehaviour {
+
+    public bool Level1Fail = false, Level2Fail = false, Level3Fail = false;
+    public Animator[] Level1Objs, Level2Objs, Level3Objs;
+    public GameObject[] Level3HideObjs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Level1Fail)
+        {
+            foreach (var temp in Level1Objs) temp.SetBool("Visible", false);
+        }
+        else
+        {
+            foreach (var temp in Level1Objs) temp.SetBool("Visible", true);
+        }
+        if(Level2Fail)
+        {
+            foreach (var temp in Level2Objs) temp.SetBool("Visible", false);
+        }
+        else
+        {
+            foreach (var temp in Level2Objs) temp.SetBool("Visible", true);
+        }
+        if (Level3Fail)
+        {
+            foreach (var temp in Level3Objs) temp.SetBool("Visible", false);
+            foreach (var temp in Level3HideObjs) temp.GetComponent<MeshRenderer>().enabled=false;
+        }
+        else
+        {
+            foreach (var temp in Level3Objs) temp.SetBool("Visible", true);
+            foreach (var temp in Level3HideObjs) temp.GetComponent<MeshRenderer>().enabled = true;
+        }
+	}
+}
